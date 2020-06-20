8fd335a47e7aa488ea4a7713059e048728c2a4c6
jfsuportedeti <jfsuportedeti@gmail.com>
2020-06-20 15:33:06 -0300

    Alterado texto e corrigidos dependencias

--- Program.cs
+++ Program.cs
@@ -6,7 +6,12 @@ namespace DotNet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World! Jose Felix Filho");
+            int vezes = 15;
+            for (int i=0; i < vezes; i++)
+            {
+             Console.WriteLine($"Hello World! Jose Felix Filho  {i}");   
+            }
         }
     }
 }
